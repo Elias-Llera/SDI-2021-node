4ea246b4bb095c9bae25d7923018454df60317e7
Elias-Llera <eliaslleragr@gmail.com>
2021-04-16 14:34:51 +0200

    SDI-2021-611-10.2-SW API REST Autenticación.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 let express = require('express');
 let app = express();
 
+let jwt = require('jsonwebtoken');
+app.set('jwt',jwt);
+
 let fs = require('fs');
 let https = require('https');
 
@@ -22,6 +25,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 let gestorBD = require("./modules/gestorBD.js");
 gestorBD.init(app, mongo);
+
+// routerUsuarioToken
+let routerUsuarioToken = express.Router();
+routerUsuarioToken.use(function(req, res, next) {
+    // obtener el token, vía headers (opcionalmente GET y/o POST).
+    let token = req.headers['token'] || req.body.token || req.query.token;
+    if (token != null) {
+        // verificar el token
+        jwt.verify(token, 'secreto', function(err, infoToken) {
+            if (err || (Date.now()/1000 - infoToken.tiempo) > 240 ){
+                res.status(403); // Forbidden
+                res.json({ acceso : false, error: 'Token invalido o caducado' });
+                // También podríamos comprobar que intoToken.usuario existe
+                return;
+            } else {
+                // dejamos correr la petición
+                res.usuario = infoToken.usuario;
+                next();
+            }
+        });
+    } else {
+        res.status(403); // Forbidden
+        res.json({
+            acceso : false,
+            mensaje: 'No hay Token'
+        });
+    }
+});
+
+// Aplicar routerUsuarioToken
+app.use('/api/cancion', routerUsuarioToken);
 
 // routerUsuarioSession
 var routerUsuarioSession = express.Router();
--- routes/rapicanciones.js
+++ routes/rapicanciones.js
@@ -87,4 +87,30 @@ module.exports = function(app, gestorBD) {
             }
         });
     });
+
+    app.post("/api/autenticar/", function(req, res){
+        let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+        let criterio= {
+            email : req.body.email,
+            password : seguro
+        }
+        gestorBD.obtenerUsuarios(criterio, function (usuarios){
+            if(usuarios == null || usuarios.length == 0) {
+                res.status(401); //Unauthorized
+                res.json({
+                    autenticado : false
+                })
+            }else{
+                let token = app.get('jwt').sign(
+                    {usuario: criterio.email , tiempo: Date.now()/1000},
+                    "secreto");
+                res.status(200);
+                res.json({
+                    autenticado : true,
+                    token : token
+                })
+            }
+        });
+    });
 }
